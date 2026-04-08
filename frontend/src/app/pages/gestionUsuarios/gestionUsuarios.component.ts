c3d4781ec4940b3237c286a11e428c181783fc7e
Paola Hernandez <hernandezvaqui@gmail.com>
2026-04-08 12:50:38 -0600

    Creacion de tabla de usuarios

--- frontend/src/app/pages/gestionUsuarios/gestionUsuarios.component.ts
+++ frontend/src/app/pages/gestionUsuarios/gestionUsuarios.component.ts
@@ -1,18 +1,58 @@
-import { Component } from "@angular/core";
-import { ButtonModule } from 'primeng/button';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
 import { InputTextModule } from 'primeng/inputtext';
-import { CheckboxModule } from 'primeng/checkbox';
-import { RouterOutlet } from '@angular/router';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { SelectModule } from 'primeng/select';
+import { Button } from "primeng/button";
+
 @Component({
-  selector: 'app-principal',
+  selector: 'app-gestion-usuarios',
   standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    TableModule,
+    TagModule,
+    InputTextModule,
+    IconFieldModule,
+    InputIconModule,
+    MultiSelectModule,
+    SelectModule,
+    Button
+],
   templateUrl: './gestionUsuarios.component.html',
-  styleUrl: './gestionUsuarios.component.css',
-  imports: [ButtonModule, InputTextModule, CheckboxModule, RouterOutlet],
+  styleUrls: ['./gestionUsuarios.component.css']
 })
+export class gestionUsuariosPage implements OnInit {
 
+  usuarios: any[] = [];
+  representatives: any[] = [];
+  statuses: any[] = [];
+  loading: boolean = false;
 
+  ngOnInit() {
 
-export class gestionUsuariosPage {
+    this.statuses = [
+      { label: 'Activo', value: 'activo' },
+      { label: 'Inactivo', value: 'inactivo' }
+    ];
 
+    this.usuarios = [];
+  }
+
+  getSeverity(status: string) {
+    switch (status) {
+      case 'activo':
+        return 'success';
+      case 'inactivo':
+        return 'danger';
+      default:
+        return 'secondary';
+    }
+  }
 }
